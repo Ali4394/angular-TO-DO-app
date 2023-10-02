7283d88b64fb0ba30cdbf1fa93b03ae84d4a24f5
Ali4394 <alitour356@gmail.com>
2023-10-02 17:48:05 +0600

    exam1

--- src/app/task-view/task-view.component.ts
+++ src/app/task-view/task-view.component.ts
@@ -1,26 +1,28 @@
-import {Component, OnInit} from '@angular/core';
-import {Task} from "../shared/models/task.model";
-import {TaskStorageService} from "../task-storage.service";
-import {Router, ActivatedRoute} from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Task } from "../shared/models/task.model";
+import { TaskStorageService } from "../task-storage.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-task-view',
-  templateUrl: './task-view.component.html',
-  styleUrls: ['./task-view.component.css']
+  selector: "app-task-view",
+  templateUrl: "./task-view.component.html",
+  styleUrls: ["./task-view.component.css"],
 })
 export class TaskViewComponent implements OnInit {
-
   task: Task;
 
-  constructor(private storage: TaskStorageService, private route: ActivatedRoute, private router: Router) {
-  }
+  constructor(
+    private storage: TaskStorageService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   /**
    *  Load task from route id parameter
    */
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.task = this.storage.get(params.get('id'));
+    this.route.paramMap.subscribe((params) => {
+      this.task = this.storage.get(params.get("id"));
     });
   }
 
@@ -31,6 +33,6 @@ export class TaskViewComponent implements OnInit {
    */
   delete(id): void {
     this.storage.delete(this.task.id);
-    this.router.navigate(['/tasks'])
+    this.router.navigate(["/tasks"]);
   }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,23 +1,21 @@
-import {Component, OnInit} from '@angular/core';
-import {TaskStorageService} from "../task-storage.service";
-import {Task} from "../shared/models/task.model";
+import { Component, OnInit } from "@angular/core";
+import { TaskStorageService } from "../task-storage.service";
+import { Task } from "../shared/models/task.model";
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  selector: "app-todo",
+  templateUrl: "./todo.component.html",
+  styleUrls: ["./todo.component.css"],
 })
 export class TodoComponent implements OnInit {
-
   tasks: Task[];
 
-  constructor(private storage: TaskStorageService) {
-  }
+  constructor(private storage: TaskStorageService) {}
 
   /**
    * Load tasks on init
    */
-  ngOnInit() : void{
+  ngOnInit(): void {
     this.storage.init();
     this.tasks = this.storage.getTasks();
   }
@@ -30,5 +28,24 @@ export class TodoComponent implements OnInit {
   delete(id): void {
     this.storage.delete(id);
     this.tasks = this.storage.getTasks();
+  }
+
+  /**
+   * Translate task status to the desired language
+   *
+   * @param status The task status to be translated
+   * @returns Translated status
+   */
+  translateStatus(status: string): string {
+    switch (status) {
+      case "not started":
+        return "еще не начата";
+      case "pending":
+        return "в ожидании";
+      case "finished":
+        return "done";
+      default:
+        return "законченный";
+    }
   }
 }
